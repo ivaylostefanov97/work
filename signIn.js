9f61746c9a30463642cca315ca3df6a83ae36096
ivaylostefanov97 <ivaylostefanov97@gmail.com>
2014-09-24 23:05:30 +0300

    Update signIn.js

--- signIn.js
+++ signIn.js
@@ -4,6 +4,43 @@
 		$("#button").click( function(){
 			var user = $("#the-username").val();
 			var password = $("#the-password").val();
+
+			function setCookie(name, value, exdays){
+				var date = new Date();
+				date.setTime(date.getTime() + (exdays*24*60*60*1000));
+				var expires = "expires=" + date.toUTCString();
+				document.cookie = name + "=" + value + ";" + expires;
+			}
+
+			function storeValues(){
+				setCookie("username", user, 1)
+				setCookie("password", password, 1)
+
+				return true;
+			}
+
+			function getCookie(name) {
+			    var names = name + "=";
+			    var ca = document.cookie.split(';');
+			    for(var i=0; i<ca.length; i++) {
+			        var c = ca[i];
+			        while (c.charAt(0)==' ') c = c.substring(1);
+			        if (c.indexOf(names) != -1) return c.substring(names.length,c.length);
+			    }
+			    return "";
+			}
+
+			function checkCookie() {
+
+			    var username = getCookie("username");
+			    var password = getCookie("password");
+
+			    if (username != "" && password != "") {
+			    	window.location = "index.html";
+			    }else{
+					window.location = "login.html"					        
+			    }
+			}
 			
 			function check(){
 				for (var i in JSON.parse(localStorage.getItem('test'))) {
@@ -11,22 +48,26 @@
 					var checkUsr = JSON.parse(localStorage.getItem('test'))[i].username;
 					var checkPass = JSON.parse(localStorage.getItem('test'))[i].password;
 
-					
-						if(user == checkUsr && password == checkPass){
-							return true
-						}
-					}
-			}
-
-				function submit(){
-					if (check() == true){
-						window.location = "index.html";
-					} else {
-						alert("Username of password is incorrect.")
+					if(user == checkUsr && password == checkPass){
+						return true
 					}
 				}
+			}
+
+			function submit(){
+				if (check() == true){
+
+				storeValues();
+				checkCookie();
+
 				
-				submit();
+
+				} else {
+					alert("Username or password is incorrect.")
+				}
+			}
+			
+			submit();
 
 		})
 
