279effc38499253b094ebac7e7d4b096d98fea71
ivaylostefanov97 <ivaylostefanov97@gmail.com>
2014-10-08 23:11:06 +0300

    Update registration.js

--- registration.js
+++ registration.js
@@ -1,19 +1,33 @@
 (function register(){
 	if(typeof(Storage) !== "undefined") {
-
 		$('#submit').click( function(){
 			var usr = $('#username').val();
 			var pass = $('#inputPassword').val();
 
-			 var test = localStorage.getItem("test");
-		     var obj = [];
-		     if(test){
-		         obj = JSON.parse(test);  
-		     }
-		     obj.push({"username":  usr, "password":pass});
-		     localStorage.setItem("test",JSON.stringify(obj));
+			function checkIfUsrExsists(){
+		    	for (var i in JSON.parse(localStorage.getItem("test"))) {
+					var get_usr = JSON.parse(localStorage.getItem("test"))[i].username;
 
-			window.location = "success.html";
+			    	if (usr == get_usr) {
+			    		alert("Username already exists!")
+			    		return false;
+			    	}
+		    	}
+		    	return true;
+		    }
+		    
+
+			if (checkIfUsrExsists() == true ) {
+				var test = localStorage.getItem("test");
+			    var obj = [];
+			    if(test){
+			         obj = JSON.parse(test);  
+			    }
+			    obj.push({"username":  usr, "password": pass});
+			    localStorage.setItem("test",JSON.stringify(obj));
+
+				window.location = "login.html";
+			}
 		})
 
 	} else {
