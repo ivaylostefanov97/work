3bd6e394eac9ac9405bda9ecf578e349f586cd2c
ivaylostefanov97 <ivaylostefanov97@gmail.com>
2014-10-08 23:10:44 +0300

    Update delete.js

--- delete.js
+++ delete.js
@@ -1,19 +1,32 @@
+
 if(typeof(Storage) !== "undefined") {
 		$(document).on('click', 'a.add', function () {
+
+			var added = $('legend').text();
 		    
 		    var self = $(this).parent();
 
-		    $(this)[0].setAttribute("id", "the-id")
+		    var that;
 
-		     var that = self.prev().prev().text();
+		     for (var i in JSON.parse(localStorage.getItem(added))){
 
-		     for (var i in JSON.parse(localStorage.getItem('test'))){
-		     	if (JSON.parse(localStorage.getItem('test'))[i].test == that) {
+		     	if (added == "Types" || added == "Networks") {
+	        		that = self.prev().prev();
+	        	}
+	        	else if(added == "Models" || added == "Computers" || added == "Network-Devices"){
+	        		that = self.prev().prev().prev().prev();
+	        		console.log(that)
+	        	}
+	        	else if(added == "Parts"){
+	        		that = self.prev().prev().prev().prev().prev().prev();
+	        	}
+
+		     	if (JSON.parse(localStorage.getItem(added))[i].added == that.text()) {
 		     		
-		     		var json = JSON.parse(localStorage["test"]);
+		     		var json = JSON.parse(localStorage[added]);
 
 		     		json.splice(i,1);
-					localStorage["test"] = JSON.stringify(json);
+					localStorage[added] = JSON.stringify(json);
 
 					var row = $(this).parent().parent();
 
