644032467cd913995f356c4060df9edba91d7c3f
ivaylostefanov97 <ivaylostefanov97@gmail.com>
2014-10-08 23:09:47 +0300

    Update change.js

--- change.js
+++ change.js
@@ -1,29 +1,56 @@
 if(typeof(Storage) !== "undefined") {
-		$(document).on('click', 'a.add', function () {
-		    
-		    // var self = $(this).parent();
+		$(document).on('click', 'a.change', function () {
 
-		    // $(this)[0].setAttribute("id", "the-id")
+			var change = '<td id="hide"><input class="form-control input-sm" type="text" id="show"></td>';
+			var added = $('legend').text();
+		    var self = $(this).parent();
+		    var that;
 
-		     // var that = self.prev().prev().text();
+			if (!$(this).attr('data-toggled') || $(this).attr('data-toggled') == 'off'){
+	           $(this).attr('data-toggled','on');
 
-		     for (var i in JSON.parse(localStorage.getItem('test'))){
-		     	if (JSON.parse(localStorage.getItem('test'))[i].test == that) {
-		     		
-		     		var json = JSON.parse(localStorage["test"]);
+	        	if(added == "Types" || added == "Networks"){
+	        		$(self.prev().prev()).prepend(change)
+	       		}
+	       		else if(added == "Models" || added == "Computers" || added == "Network-Devices"){
+	       			$(self.prev().prev().prev().prev()).prepend(change)
+	       		}
+	       		else if(added == "Parts"){
+	       			$(self.prev().prev().prev().prev().prev().prev()).prepend(change)
+	       		}
+		    }
+		    else if ($(this).attr('data-toggled') == 'on'){
+		           $(this).attr('data-toggled','off');
 
-		     		JSON.parse(localStorage["test"])[i].test = $('#inputDefault').val();
+		        var json = JSON.parse(localStorage[added]);
+		        var has = $('#show').val();
 
-		     		
-					// localStorage["test"] = JSON.stringify(json);
+		        for (var i in JSON.parse(localStorage.getItem(added))){
 
+		        	if (added == "Types" || added == "Networks") {
+		        		that = self.prev();
+		        	}
+		        	else if(added == "Models" || added == "Computers" || added == "Network-Devices"){
+		        		that = self.prev().prev().prev();
+		        		console.log(that)
+		        	}
+		        	else if(added == "Parts"){
+		        		that = self.prev().prev().prev().prev().prev();
+		        	}
 
+			     	if (JSON.parse(localStorage.getItem(added))[i].added == that.text()) {
+			     		
+			     		json[i].added = has;
+			     		
+			     		localStorage.setItem(added, JSON.stringify(json));
 
-		     	}
-		     }
+						that.html(has)
+						console.log(self.prev().html())
+					}
+				}
 
+		        $('#hide').remove();
+		    }
 		});
 	} else {
     	alert("Local Storage it not supported")
-}
-
