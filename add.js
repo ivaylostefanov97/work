e2dab2e9e8d8b2a3015fb841bda1d744fba1ff49
ivaylostefanov97 <ivaylostefanov97@gmail.com>
2014-10-08 23:08:32 +0300

    Update add.js

--- add.js
+++ add.js
@@ -1,31 +1,156 @@
-if(typeof(Storage) !== "undefined") {
-    (function(){
-		$('#add').click(function(){
-			var input = $('#inputDefault').val();
+ var obj = [];
+			    var type = $("#type").val();
+			    var type1 = $("#type-1").val();
+			    var theType = "type";
 
-			var test = localStorage.getItem("test");
-		     var obj = [];
-		     if(test){
-		         obj = JSON.parse(test);  
-		     }
-		     obj.push({"test":  input});
-		     localStorage.setItem("test",JSON.stringify(obj));
+			    if(add){
+			        obj = JSON.parse(add);  
+			    }
 
-		    for (var i in JSON.parse(localStorage.getItem('test'))) {
-				var get = JSON.parse(localStorage.getItem('test'))[i].test;
-			}
+			    function checkIfExsists(){
+			    	for (var i in JSON.parse(localStorage.getItem(added))) {
+						var get = JSON.parse(localStorage.getItem(added))[i].added;
 
-			function table(){
-        		$('#wrapper').prepend("<tr><td>" + get +  "</td><td><a href='#' class='btn btn-primary btn-xs' >change</a></td><td><a href='#' class='btn btn-primary btn-xs add'>delete</a></td></tr>" )
-			}
+				    	if (input == get) {
+				    		alert("Element exists!")
+				    		return false;
+				    	}
+				    	else if(input == ""){
+				    		alert("Input is empty!")
+				    		return false;
+				    	}
+			    	}
+			    	return true;
+			    }
 
-			table();
-		})
+			    if (added == "Types"){
 
-	}())
-} else {
-    alert("Local Storage it not supported")
-	}
+			    	if(checkIfExsists() == true){
 
-	
-	
+				    	obj.push({added: input});
+						localStorage.setItem(added,JSON.stringify(obj));
+
+				    	for (var i in JSON.parse(localStorage.getItem(added))) {
+							var get = JSON.parse(localStorage.getItem(added))[i].added;
+							var pk = i;
+						}
+						$('#wrapper').prepend("<tr><td>" + pk + "<td><td>" + get +  inner );
+					}
+				}
+				else if (added == "Models"){
+
+					if(checkIfExsists() == true){
+
+						for (var i in JSON.parse(localStorage.getItem("Types"))) {
+							var get_item = JSON.parse(localStorage.getItem("Types"))[i].added;
+
+							if (type == get_item) {
+								obj.push({added: input, Type: get_item});
+								localStorage.setItem(added,JSON.stringify(obj));
+						
+
+								for (var i in JSON.parse(localStorage.getItem(added))) {
+									var get = JSON.parse(localStorage.getItem(added))[i].added;
+									var pk = i;
+								}
+								$('#wrapper').prepend("<tr><td>" + pk + "<td><td>" + get + "<td><td>" + get_item + inner )
+							}
+						}
+					}
+				}
+				else if (added == "Networks")	{
+
+					if(checkIfExsists() == true){
+
+						obj.push({added: input});
+						localStorage.setItem(added,JSON.stringify(obj));
+
+				    	for (var i in JSON.parse(localStorage.getItem(added))) {
+							var get = JSON.parse(localStorage.getItem(added))[i].added;
+							var pk = i;
+						}
+						$('#wrapper').prepend("<tr><td>" + pk + "<td><td>" + get +  inner );
+					}
+				}
+				else if (added == "Network-Devices"){
+
+					if(checkIfExsists() == true){
+
+						for (var i in JSON.parse(localStorage.getItem("Networks"))) {
+							var get_item = JSON.parse(localStorage.getItem("Networks"))[i].added;
+
+							if (type == get_item) {
+								obj.push({added: input, Type: get_item});
+								localStorage.setItem(added,JSON.stringify(obj));
+						
+
+								for (var i in JSON.parse(localStorage.getItem(added))) {
+									var get = JSON.parse(localStorage.getItem(added))[i].added;
+									var pk = i;
+								}
+							}
+				   		}
+				   		$('#wrapper').prepend("<tr><td>" + pk + "<td><td>" + get + "<td><td>" + get_item + inner )
+			   		}
+			   	}
+
+			   	else if (added == "Computers"){
+
+			   		if(checkIfExsists() == true){
+
+				   		for (var i in JSON.parse(localStorage.getItem("Networks"))) {
+							var get_item = JSON.parse(localStorage.getItem("Networks"))[i].added;
+
+							if (type == get_item) {
+								obj.push({added: input, Type: get_item});
+								localStorage.setItem(added,JSON.stringify(obj));
+						
+
+								for (var i in JSON.parse(localStorage.getItem(added))) {
+									var get = JSON.parse(localStorage.getItem(added))[i].added;
+									var pk = i;
+									arr.push(pk)
+								}
+							}
+				   		}
+				   		$('#wrapper').prepend("<tr><td>" + pk + "<td><td>" + get + "<td><td>" + get_item + inner )
+			   		}
+			   	}
+
+				else if (added == "Parts"){
+
+					if(checkIfExsists() == true){
+
+						$.each(JSON.parse(localStorage.getItem("Models")), function(i, v){
+
+									for (var i in JSON.parse(localStorage.getItem("Computers"))) {
+
+										var get_item_1 = JSON.parse(localStorage.getItem("Computers"))[i].added;
+
+										for (var i in JSON.parse(localStorage.getItem("Network-Devices"))) {
+
+										var get_item_2 = JSON.parse(localStorage.getItem("Network-Devices"))[i].added;
+
+									if (type == v.added && type1 == get_item_1 || type1 == get_item_2) {
+
+										obj.push({added: input, Type: type, Device: type1});
+										localStorage.setItem(added,JSON.stringify(obj));
+
+										for (var s in JSON.parse(localStorage.getItem(added))) {
+											var get = JSON.parse(localStorage.getItem(added))[s].added;
+											var pk = s;
+										}
+
+							   			$('#wrapper').prepend("<tr><td>" + pk + "<td><td>" + get + "<td><td>" + type + "<td><td>" + type1 + inner )
+									}
+								}
+							}
+						})
+					}
+				}
+			})
+		}())
+	} else {
+	    alert("Local Storage it not supported")
+		}
+})
